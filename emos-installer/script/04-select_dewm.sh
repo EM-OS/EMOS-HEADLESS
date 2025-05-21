9cfcdd4719d34753686e38fe942272ed14536e17
EasyCanadianGamer <eyadi2005@gmail.com>
2025-05-21 06:03:27 +0000

    test

--- emos-installer/script/04-select_dewm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-de=$(gum choose "Awesome" "Bspwm" "Budgie" "Gnome" "Hyprland" "I3" "Plasma" "Xfce4" "None")
-echo "$de" > /tmp/emos_de
-
--- profiledef.sh
+++ profiledef.sh
@@ -13,6 +13,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+packages+=("emos-installer")
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
